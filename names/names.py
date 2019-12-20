3d8f6adc1ef0b0d08af98c15cedc5531d6006e8d
Scott Renz <scottrenz@scottrenz.com>
2019-12-20 13:35:15 -0500

    sped up names

--- names/names.py
+++ names/names.py
@@ -11,10 +11,62 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+alln =  names_1 + names_2
+def merge( arrA, arrB ):
+    #elements = len( arrA ) + len( arrB )
+    #merged_arr = [0] * elements
+    # TO-DO
+    merged_arr = []
+    while arrA != [] and arrB != []:
+        if arrA[0] <= arrB[0]:
+            merged_arr.append(arrA[0])
+            if len(arrA) == 1:
+                arrA = []
+            else:    
+                arrA = arrA[1:]
+        else:
+            merged_arr.append(arrB[0])
+            if len(arrB) == 1:
+                arrB = []
+            else:    
+                arrB = arrB[1:]
+    while arrA != []:
+        merged_arr.append(arrA[0])
+        arrA.pop(0)
+        # if len(arrA) == 1:
+        #     arrA = []
+        # else:    
+        #     arrA = arrA[1:]
+    while arrB != []:
+        merged_arr.append(arrB[0])
+        arrB.pop(0)
+        # if len(arrB) == 1:
+        #     arrB = []
+        # else:    
+        #     arrB = arrB[1:]
+    return merged_arr
+
+def merge_sort( arr ):
+    # TO-DO
+    if len(arr) < 2:
+        return arr
+    left = arr[:len(arr)//2]
+    right = arr[len(arr)//2:]
+    left = merge_sort(left)
+    right =merge_sort(right)
+    arr = merge(left,right)
+    return arr
+    
+x = ''
+for v in merge_sort(alln):
+    if v == x:
+        duplicates.append(v)
+    x = v    
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
